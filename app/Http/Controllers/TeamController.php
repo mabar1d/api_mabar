b7284ba94e5423d0a4f6c594a13f4e770e12ad2d
Vidi Wijaya <vidya.wijaya92@gmail.com>
2023-01-29 23:23:38 +0700

    [BUGFIX] setelah decline req masih muncul

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -486,6 +486,7 @@ class TeamController extends Controller
                 if ($checkTeamExist) {
                     if ($checkTeamExist->admin_id == $userId) {
                         $listReqJoinTeam = MasterReqJoinTeam::where('team_id', $teamId)
+                            ->whereNull("status")
                             ->get();
                         if ($listReqJoinTeam->first()) {
                             $resultData = array();
