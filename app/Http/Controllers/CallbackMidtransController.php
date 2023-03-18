f87e5970cedf662734550d79cd330c208c7c697e
Vidi Wijaya <vidya.wijaya92@gmail.com>
2023-03-18 19:35:40 +0700

    pake slash

--- app/Http/Controllers/CallbackMidtransController.php
+++ app/Http/Controllers/CallbackMidtransController.php
@@ -41,6 +41,7 @@ class CallbackMidtransController extends Controller
 
         // get the HTTP POST body of the request
         $request_body = file_get_contents('php://input');
+        dd($request_body);
         // set response's content type as JSON
         header('Content-Type: application/json');
         // call charge API using request body passed by mobile SDK
